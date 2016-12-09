60afa39c5a6a20f5314efab204eda607532a4740
Gaukler Faun <baumann.juergen@posteo.de>
2016-12-09 21:01:03 +0100

    bugfix hidden settings menu

--- app/src/main/java/de/baumann/hhsmoodle/HHS_MainScreen.java
+++ app/src/main/java/de/baumann/hhsmoodle/HHS_MainScreen.java
@@ -214,7 +214,7 @@ public class HHS_MainScreen extends AppCompatActivity {
     public boolean onPrepareOptionsMenu(Menu menu) {
 
         if (sharedPref.getBoolean ("help", false)){
-            menu.getItem(4).setVisible(false); // here pass the index of save menu item
+            menu.getItem(5).setVisible(false); // here pass the index of save menu item
         }
         return super.onPrepareOptionsMenu(menu);
     }
